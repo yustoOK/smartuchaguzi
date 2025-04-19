8cbe8bbad9381737785d63413d28f9db7e099764
yustoOK <yustobitalio24@gmail.com>
2025-04-19 10:23:50 +0300

    Contracts Deployment

--- api/blockchain/scripts/deploy.js
+++ api/blockchain/scripts/deploy.js
@@ -6,13 +6,13 @@ async function main() {
 
     const ElectionContract = await hre.ethers.getContractFactory("ElectionContract");
     const electionContract = await ElectionContract.deploy();
-    await electionContract.deployed();
-    console.log("ElectionContract deployed to:", electionContract.address);
+    await electionContract.waitForDeployment();
+    console.log("ElectionContract deployed to:", await electionContract.getAddress());
 
     const VoteContract = await hre.ethers.getContractFactory("VoteContract");
-    const voteContract = await VoteContract.deploy(electionContract.address);
-    await voteContract.deployed();
-    console.log("VoteContract deployed to:", voteContract.address);
+    const voteContract = await VoteContract.deploy(await electionContract.getAddress());
+    await voteContract.waitForDeployment();
+    console.log("VoteContract deployed to:", await voteContract.getAddress());
 }
 
 main().catch((error) => {
