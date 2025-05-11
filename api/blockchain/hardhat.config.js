8b07a3fda84c5cc21e3805018ff6653638f8a85e
yustoOK <yustobitalio24@gmail.com>
2025-05-11 11:33:59 +0300

    pheew

--- api/blockchain/hardhat.config.js
+++ api/blockchain/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
