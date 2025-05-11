ba7b002efc4415260cf0f35732a914756b852e87
yustoOK <yustobitalio24@gmail.com>
2025-05-11 12:10:22 +0300

    pheew

--- api/blockchain/scripts/1_deploy.js
+++ api/blockchain/scripts/1_deploy.js
@@ -2,13 +2,18 @@ const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Deploying VoteContract with account:", deployer.address);
 
+  console.log("Deploying VoteContract...");
   const VoteContract = await hre.ethers.getContractFactory("VoteContract");
   const voteContract = await VoteContract.deploy();
 
-  await voteContract.deployed();
-  console.log("VoteContract deployed to:", voteContract.address);
+  // Wait for the deployment transaction to be mined
+  const txReceipt = await voteContract.deploymentTransaction().wait();
+  console.log("Transaction receipt:", txReceipt);
+
+  // Use txReceipt.contractAddress to get the deployed address
+  console.log("VoteContract deployed to:", txReceipt.contractAddress);
 }
 
 main().catch((error) => {
