3421dee82cbb9aa4386aa2e59f0e69667292ffb2
yustoOK <yustobitalio24@gmail.com>
2025-05-11 13:33:31 +0300

    pheew

--- 2fa/vendor/autoload.php
+++ 2fa/vendor/autoload.php
@@ -6,7 +6,7 @@ if (PHP_VERSION_ID < 50600) {
     if (!headers_sent()) {
         header('HTTP/1.1 500 Internal Server Error');
     }
-    $err = 'Composer 2.3.0 dropped support for autoloading on PHP <5.6 and you are running '.PHP_VERSION.', please upgrade PHP or use Composer 2.2 LTS via "composer self-update --2.2". Aborting.'.PHP_EOL;
+    $err = 'Composer 2.3.0 dropped support for autoloading on PHP <5.6 and you are running ' . PHP_VERSION . ', please upgrade PHP or use Composer 2.2 LTS via "composer self-update --2.2". Aborting.' . PHP_EOL;
     if (!ini_get('display_errors')) {
         if (PHP_SAPI === 'cli' || PHP_SAPI === 'phpdbg') {
             fwrite(STDERR, $err);
@@ -17,6 +17,5 @@ if (PHP_VERSION_ID < 50600) {
     throw new RuntimeException($err);
 }
 
-require_once __DIR__ . '/composer/autoload_real.php';
-
+require_once __DIR__ . './composer/autoload_real.php';
 return ComposerAutoloaderInit9052ceee78e7d3c863f8ccf6d9e7caf0::getLoader();
